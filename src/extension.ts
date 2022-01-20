00f2429981e99d0b0aa3b18dc0e844f039ef4f99
Petrov Kirill <hoffusha@gmail.com>
2022-01-20 18:04:59 +0300

    Was added quick links for navigation

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,19 @@
 import * as vscode from 'vscode';
-import { NavigationProvider } from './navigation';
+import { NavigationProvider, TreeItem } from './navigation';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "navigationgift" is now active!');
 	const editor = vscode.window.activeTextEditor;
     if (editor) {
         const document = editor.document;
-		vscode.window.registerTreeDataProvider('navigation', new NavigationProvider(document.getText()));
+		vscode.window.registerTreeDataProvider('navigation', new NavigationProvider(textAdaptaion(document.getText())));
     }
 
-	const disposable = vscode.commands.registerCommand('navigationgift.goToLine', () => {
+	const disposable = vscode.commands.registerCommand('navigationgift.goToLine', (item: TreeItem) => {
         if(editor) {
-			const range = editor.document.lineAt(99).range;
+			const range = editor.document.lineAt(item.line - 1).range;
 			editor.revealRange(range);
-			editor.selection = new vscode.Selection(range.start, range.start);
+			editor.selection = new vscode.Selection(range.start, range.end);
 		}
     });
 
@@ -21,3 +21,31 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 export function deactivate() {}
+
+function textAdaptaion(documentText: string) {
+	let splitted = documentText.split(/\n\s*\n/);
+	splitted = splitted.map(
+		(capture) => capture.split(/\n/)
+			.filter((line) => !line.startsWith('//'))
+			.reduce((section, line) => section + '\n' + line, '')
+			.trim()
+	);
+	splitted = splitted.filter((capture) => capture !== '');
+
+	let linesAndQuestions = new Map();
+	documentText.split(/\n/).forEach((line, i) => {
+		for(let n = 0; n < splitted.length; n++) {
+			if(line.startsWith(splitted[n]) || splitted[n].startsWith(line)) {
+				linesAndQuestions.set(i + 1, splitted[n]
+					.replace(/{[\s\S]*}/, '___')
+					.replace(/___$/, '')
+					.replace(/(?<=::[\s\S]*::)[\s\S]*/, '')
+					.replace(/::/g, '')
+				);
+				splitted.splice(n, 1);
+				break;
+			}
+		}
+	});
+	return linesAndQuestions;
+}
--- src/navigation.ts
+++ src/navigation.ts
@@ -3,38 +3,10 @@ import * as vscode from 'vscode';
 export class NavigationProvider implements vscode.TreeDataProvider<TreeItem> {
   onDidChangeTreeData?: vscode.Event<TreeItem|null|undefined>|undefined;
 
-  data: TreeItem[];
+  data: TreeItem[] = [];
 
-  constructor(documentText: String) {
-    let splitted = documentText.split(/\n\s*\n/);
-    splitted = splitted.map(
-        (capture) => capture.split(/\n/)
-            .filter((line) => !line.startsWith('//'))
-            .reduce((section, line) => section + '\n' + line, '')
-            .trim()
-    );
-    splitted = splitted.filter((capture) => capture !== '');
-    splitted = splitted.map(
-        (capture) => capture
-            .replace(/{[\s\S]*}/, '___')
-            .replace(/___$/, '')
-            .replace(/(?<=::[\s\S]*::)[\s\S]*/, '')
-            .replace(/::/g, '')
-    );
-    splitted.forEach((capture, i) => console.log(`Capture #${i}:\n${capture}`));
-    this.data = [];
-    splitted.forEach((capture) => this.data.push(new TreeItem(capture)));
-
-
-
-    //Artificial data, for testing only
-
-    // this.data = [new TreeItem('cars', [
-    //   new TreeItem(
-    //       'Ford', [new TreeItem('Fiesta'), new TreeItem('Focus'), new TreeItem('Mustang')]),
-    //   new TreeItem(
-    //       'BMW', [new TreeItem('320'), new TreeItem('X3'), new TreeItem('X5')])
-    // ])];
+  constructor(headers: Map<number, string>) {
+    headers.forEach((capture, line) => this.data.push(new TreeItem(capture, line)));
   }
 
   getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
@@ -49,19 +21,22 @@ export class NavigationProvider implements vscode.TreeDataProvider<TreeItem> {
   }
 }
 
-class TreeItem extends vscode.TreeItem {
+export class TreeItem extends vscode.TreeItem {
   children: TreeItem[]|undefined;
+  line: number;
 
   command = {
     "title": "Go to line",
     "command": "navigationgift.goToLine",
+    "arguments": [this]
   }
 
-  constructor(label: string, children?: TreeItem[]) {
+  constructor(label: string, line: number, children?: TreeItem[]) {
     super(
         label,
         children === undefined ? vscode.TreeItemCollapsibleState.None :
                                  vscode.TreeItemCollapsibleState.Expanded);
     this.children = children;
+    this.line = line;
   }
 }
