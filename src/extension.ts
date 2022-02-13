cef22163455372e1dd2b3e8a9ba8bcad11d7964c
Petrov Kirill <hoffusha@gmail.com>
2022-02-13 16:33:12 +0300

    Wrong format files will not open in extension anymore

--- src/extension.ts
+++ src/extension.ts
@@ -50,8 +50,11 @@ function textAdaptaion(documentText: string): Map<number, string> {
 
 function updateTreeView(navigationProvider: NavigationProvider): void {
 	const activeEditor = vscode.window.activeTextEditor;
-	if(activeEditor) {
+	if(activeEditor && activeEditor.document.fileName.endsWith('.gift')) {
 		navigationProvider.updateData(textAdaptaion(activeEditor.document.getText()));
+		navigationProvider.refresh();
+	} else {
+		navigationProvider.updateData(textAdaptaion(''));
 		navigationProvider.refresh();
 	}
 }
